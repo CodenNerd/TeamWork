0c6a71ff96f46c4b42e5bf471636a73d06e703cf
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-04 15:28:42 +0100

    install body-parser

--- Controllers/addUser.js
+++ Controllers/addUser.js
@@ -1,11 +1,12 @@
 import pool from '../Models/queries';
+import uuid from 'uuid/v1';
 
 const addUser = (req, res) => {
 
     const query = `INSERT INTO teamwork.users(ID, firstName, lastName, email, password, gender, jobRole, department, address, datetime) VALUES ($1,$2, $3,$4, $5, $6, $7, $8)`
     const { firstName, lastName, email, password, gender, jobRole, department, address } = req.body;
     const params = [
-        UUID,
+        uuid(),
         firstName, 
         lastName, 
         email, 
@@ -22,6 +23,7 @@ const addUser = (req, res) => {
                 return res.status(202).send({
                     status: "success",
                     data: {
+                        params,
                         message: "User account successfully created",
                         token: "String",
                         userId: "Integer",
@@ -33,7 +35,10 @@ const addUser = (req, res) => {
         })
         .catch(e => {
             console.error(e);
-            res.send(e)
+            res.send({
+                params,
+                e
+            })
         })
 
 
