ad2003038eb465ae518861e1a96ecbfbd8d14a78
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-05 07:57:36 +0100

    query database to ensure email does not already exist

--- Controllers/addUser.js
+++ Controllers/addUser.js
@@ -3,20 +3,34 @@ import uuid from 'uuid/v1';
 import schema from '../Models/joiSchema';
 
 const addUser = (req, res) => {
-
-    //validation
-
-    
-    const query = `INSERT INTO teamwork.users(ID, firstName, lastName, email, password, gender, jobRole, department, address, datetime) VALUES ($1,$2, $3,$4, $5, $6, $7, $8, $9, $10)`
     const { firstName, lastName, email, password, gender, jobRole, department, address } = req.body;
     
-   schema.validate({
+   const validate  = schema.validate({
         firstName,
         lastName,
         email,
         address
-    })
+    });
+    
+    if(validate.error){
+        return res.status(400).send({
+            error: "validation error",
+            message: validate.error.details[0].message
+        })
+    }
 
+     const checkDBQuery = `SELECT * from teamwork.users WHERE email = $1`;
+
+
+     pool.query(checkDBQuery, [email])
+    .then(result =>{
+        if(result.rowCount){
+            return res.status(400).send({
+                message: `A user with email:${email} already exists`,
+                result
+            })
+        }
+        const query = `INSERT INTO teamwork.users(ID, firstName, lastName, email, password, gender, jobRole, department, address, datetime) VALUES ($1,$2, $3,$4, $5, $6, $7, $8, $9, $10)`
     
     const params = [
         uuid(),
@@ -36,7 +50,6 @@ const addUser = (req, res) => {
                 return res.status(202).send({
                     status: "success",
                     data: {
-                        params,
                         message: "User account successfully created",
                         token: "String",
                         userId: params[0],
@@ -55,28 +68,17 @@ const addUser = (req, res) => {
             });
             
         })
+    })
+     .catch(e=>{
+         return res.status(500).send({
+             e
+         })
+        console.error(e);
+     })
 
+   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
 }
 
