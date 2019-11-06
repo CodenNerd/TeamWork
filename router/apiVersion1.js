93f45713dd078816f172a6f570923192ca80799f
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-06 10:04:03 +0100

    create post gif route in router

--- router/apiVersion1.js
+++ router/apiVersion1.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import addUser from '../Controllers/addUser';
 import Auth from '../Middleware/Auth';
-import signIn from '../Controllers/signIn'
+import signIn from '../Controllers/signIn';
+import createGif from '../Controllers/createGif';
 
 const api = Router();
 
 api.post('/auth/create-user', Auth, addUser);
-api.post('/auth/signin', Auth, signIn)
+api.post('/signin', signIn);
+api.post('/auth/gifs', Auth, createGif)
 
 export default api;
