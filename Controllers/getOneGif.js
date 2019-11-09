54da35340668a0752fcf9cf0c0c7b52422eedc32
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-09 15:12:57 +0100

    correct error message

--- Controllers/getOneGif.js
+++ Controllers/getOneGif.js
@@ -4,19 +4,19 @@ const getPost = {
     async getPost(req, res) {
         // if (req.user.userType !== 'employee') return res.status(401).send({ message: 'please create an employee account to perform this task' });
 
-        const query = `SELECT * FROM teamwork.articles  WHERE articleid = $1`
+        const query = `SELECT * FROM teamwork.gifs  WHERE gifid = $1`
         try {
-            const { rows } = await pool.query(query, [req.params.articleId]);
+            const { rows } = await pool.query(query, [req.params.gifId]);
             if (!rows[0]) {
                 return res.status(404).send({
                     status: `error`,
-                    message: `Article not found`
+                    message: `Gif not found.`
                 })
             }
             let comments;
             try {
-                const commentQuery = `SELECT commentId, commentBody, commentAuthorId FROM teamwork.comments WHERE commentpostid=$1 AND commentposttype='article'`
-                const { rows } = await pool.query(commentQuery, [req.params.articleId]);
+                const commentQuery = `SELECT commentId, commentBody, commentAuthorId FROM teamwork.comments WHERE commentpostid=$1 AND commentposttype='gif'`
+                const { rows } = await pool.query(commentQuery, [req.params.gifId]);
                 if (rows[0]) {
                     comments = rows
                 }else{
@@ -33,17 +33,17 @@ const getPost = {
             return res.status(200).send({
                 status: `success`,
                 data: {
-                    id: rows[0].articleid,
+                    id: rows[0].gifid,
                     createdOn: rows[0].datetime,
                     title: rows[0].title,
-                    article: rows[0].articlebody,
+                    url: rows[0].imageurl,
                     comments
                     }
                 })
         } catch (error) {
             res.status(500).send({
                 status: `error`,
-                message: `Server error. Could not get article`
+                message: `Server error. Could not get gif`
             })
         }
     }
