f54669ad5935298e37b470b80481484307d6cfd4
AbdulAzeez Atanda <aatanda.dammy@gmail.com>
2019-11-11 07:08:52 +0100

    write function to retrieve article in a category

---
+++ Controllers/getTagArticle.js
@@ -0,0 +1,29 @@
+import pool from '../Models/queries';
+
+const getPosts = {
+    async getPosts(req, res) {
+         // if (req.user.userType !== 'employee') return res.status(401).send({ message: 'please create an employee account to perform this task' });
+
+        const query = `SELECT * FROM teamwork.articles  WHERE tag = $1`
+        try {
+            const { rows } = await pool.query(query, [req.params.tag]);
+            if (!rows[0]) {
+                return res.status(404).send({
+                    status: `error`,
+                    message: `No article in this category yet.`
+                })
+            }
+
+            return res.status(200).send({
+                status: `success`,
+                data: rows
+            })
+        } catch (error) {
+            return res.status(500).send({
+                status: `error`,
+                message: `Server error. Could not get article`
+            })
+        }
+    }
+}
+export default getPosts.getPosts;
